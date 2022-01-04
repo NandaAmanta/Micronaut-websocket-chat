5d7b6bf825cc8c8e8de958ef8f74917f7f4d70b8
Nanda Amanta <nansamgr@gmail.com>
2022-01-05 00:07:40 +0800

    Add Log

--- src/main/java/com/nanda/websocket/ChatWebSocketServer.java
+++ src/main/java/com/nanda/websocket/ChatWebSocketServer.java
@@ -19,11 +19,15 @@ import com.nanda.payloadModel.MessageRequest;
 import io.micronaut.websocket.WebSocketBroadcaster;
 import io.micronaut.websocket.WebSocketSession;
 import io.micronaut.websocket.annotation.OnClose;
+import io.micronaut.websocket.annotation.OnError;
 import io.micronaut.websocket.annotation.OnMessage;
 import io.micronaut.websocket.annotation.OnOpen;
 import io.micronaut.websocket.annotation.ServerWebSocket;
+import java.text.MessageFormat;
 import java.util.function.Predicate;
 import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -31,6 +35,8 @@ import javax.validation.Valid;
  */
 @ServerWebSocket("/chat/{topic}/{username}")
 public class ChatWebSocketServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ChatWebSocketServer.class);
 
     private final WebSocketBroadcaster broadcaster;
 
@@ -40,20 +46,28 @@ public class ChatWebSocketServer {
 
     @OnOpen
     public void onOpen(String topic, String username, WebSocketSession session) {
+        LOG.info("{} joined the room chat.", username);
         String msg = username + " Joined Ws!";
         broadcaster.broadcastSync(msg, isValid(topic));
     }
 
     @OnMessage
     public void onMessage(String topic, String username, @Valid MessageRequest message, WebSocketSession session) {
+        LOG.info("Message arrived from : {}", username);
         String msg = username + " : " + message.getMessage();
         broadcaster.broadcastSync(msg, isValid(topic));
     }
 
     @OnClose
     public void onClose(String topic, String username, WebSocketSession session) {
+        LOG.info("{} has leaved the chat room",username);
         String msg = username + " has leaved ws !";
         broadcaster.broadcastSync(msg, isValid(topic));
+    }
+    
+    @OnError
+    public void onError(){
+        LOG.error("Unknown Error found");
     }
 
     private Predicate<WebSocketSession> isValid(String topic) {
